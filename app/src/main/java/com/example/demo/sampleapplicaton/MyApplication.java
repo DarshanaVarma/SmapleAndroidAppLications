25a3a8f3b8c49ab71fcf902f78499875892abe98
DarshanaVarma <darshana.cse11@gmail.com1>
2017-10-27 12:52:30 +0530

    Dagger2 with mvp

--- app/src/main/java/com/example/demo/sampleapplicaton/MyApplication.java
+++ app/src/main/java/com/example/demo/sampleapplicaton/MyApplication.java
@@ -1,7 +1,20 @@
 package com.example.demo.sampleapplicaton;
 
+import android.app.Activity;
 import android.app.Application;
+import android.support.annotation.NonNull;
 
+import com.example.demo.sampleapplicaton.Config.ApiInterface;
+import com.example.demo.sampleapplicaton.Config.SharedUtils;
+import com.example.demo.sampleapplicaton.dagger.components.DaggerMainComponent;
+import com.example.demo.sampleapplicaton.dagger.components.MainComponent;
+import com.example.demo.sampleapplicaton.dagger.modules.ContextModule;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import okhttp3.OkHttpClient;
 
 
 /**
@@ -12,13 +25,70 @@ public class MyApplication extends Application {
 
 
 
+
+
+    public static MyApplication get(Activity activity) {
+        return (MyApplication) activity.getApplication();
+    }
+
+    private Picasso picasso;
+    private ApiInterface apiInterface;
+    private SharedUtils utils;
+    private MainComponent component;
+    private static MyApplication mInstance;
+    private OkHttpClient okHttpClient;
+
+    private static final String TWITTER_KEY = "GfmsWZ5AY4bpwWXcrxUWZNEdm";
+    private static final String TWITTER_SECRET = "KL6udJr0KK3yDsFvqrMKRs3sOGNM0ybUtmzH4norHwrnljQm8Y";
+
+
     @Override
     public void onCreate() {
         super.onCreate();
+         component= DaggerMainComponent.builder().contextModule(new ContextModule(this)).build();
+
+
+        apiInterface= component.getApiInterface();
+        picasso=component.getPicasso();
+        utils=component.getUtils();
+        okHttpClient = component.getOkHttpClient();
+        mInstance=this;
+
+
+
+
+
+    }
+
+
+    public MainComponent getComponent(){
+        return component;
+    }
+
+    public ApiInterface getApiInterface(){
+        return apiInterface;
+    }
+
+    public Picasso getPicasso(){
+        return picasso;
+    }
+
+    public SharedUtils getSharedUtils(){
+        return utils;
+    }
+    public OkHttpClient okHttpClient(){
+        return okHttpClient;
+    }
+
+
+    public static synchronized MyApplication getInstance() {
+        return mInstance;
+    }
+
 
 //            component= DaggerAppComponent.builder().contextModule(new ContextModule(this)).build();
 
 
     }
 
-}
+
--- app/src/main/java/com/example/demo/sampleapplicaton/activities/LandingActivity.java
+++ app/src/main/java/com/example/demo/sampleapplicaton/activities/LandingActivity.java
@@ -3,14 +3,50 @@ package com.example.demo.sampleapplicaton.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.example.demo.sampleapplicaton.MyApplication;
 import com.example.demo.sampleapplicaton.R;
+import com.example.demo.sampleapplicaton.dagger.components.DaggerLandingActivityComponent;
+import com.example.demo.sampleapplicaton.dagger.components.LandingActivityComponent;
+import com.example.demo.sampleapplicaton.dagger.components.MainComponent;
+import com.example.demo.sampleapplicaton.dagger.modules.LandingActivityModule;
+import com.example.demo.sampleapplicaton.models.UserDetailPojo;
+import com.example.demo.sampleapplicaton.presenters.LandingPresenter;
+import com.example.demo.sampleapplicaton.views.LandingView;
 
-public class LandingActivity extends AppCompatActivity {
+import javax.inject.Inject;
 
+public class LandingActivity extends AppCompatActivity implements LandingView {
+    @Inject
+    LandingPresenter presenter;
+
+    LandingActivityComponent component;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+
+        component= DaggerLandingActivityComponent.builder().
+                landingActivityModule(new LandingActivityModule(this)).
+                mainComponent(MyApplication.get(this).getComponent()).build();
+        component.injectLandingActivity(this);
+
+
+        presenter.getUsetDetails(7);
+
+    }
+
+    @Override
+    public void userDetail(UserDetailPojo body) {
+            body.getProfilepic();
+    }
+
+    @Override
+    public void showMessage(String message) {
+
+    }
+
+    @Override
+    public void redirectToWebsite(String webUrl) {
 
     }
 }
